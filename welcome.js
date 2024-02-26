a749baa68fc0c74ea6ff384abf22224cde227d99
beeenz222 <145907077+beeenz222@users.noreply.github.com>
2024-02-26 21:07:26 +0100

    Delete welcome.js

--- welcome.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("logoutLink").addEventListener("click", function(event) {
-    event.preventDefault();
-    // Clear the session storage and redirect to the login page on logout
-    sessionStorage.removeItem("loggedIn");
-    window.location.href = "index.html";
-  });
-  
